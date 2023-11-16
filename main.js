fb2dff6b3f7f6c54d61f197f7b3785891c89614e
Bikra123 <149250651+Bikra123@users.noreply.github.com>
2023-11-17 04:01:24 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,287 @@
+let isClicked = false
+const btn1 = document.querySelector('.btn1')
+const btn2 = document.querySelector('.btn2')
+const btn3 = document.querySelector('.btn3')
+const btn4 = document.querySelector('.btn4')
+const btn5 = document.querySelector('.btn5')
+const btn6 = document.querySelector('.btn6')
+const btn7 = document.querySelector('.btn7')
+const btn8 = document.querySelector('.btn8')
+const btn9 = document.querySelector('.btn9')
+const btn10 = document.querySelector('.btn10')
+const btn11 = document.querySelector('.btn11')
+const btn12 = document.querySelector('.btn12')
+const hasil = document.querySelector('.hasil-kopipasta')
+let paragrafBaru = document.createElement('p')
+let arrayParagrafBaru = [
+    'WANGY WANGY WANGY WANGY WANGY WANGY WANGY ',
+    'Crot Crot Crot Crot Crot Crot Crot Crot Crot Crot Crot Crot Crot Crot Crot Crot Crot Crot Crot ',
+    'Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp',
+    'Isep Isep Isep Isep Isep Isep Isep Isep Isep Isep Isep Isep Isep Isep Isep Isep Isep Isep Isep',
+    'Jilat Jilat Jilat Jilat Jilat Jilat Jilat Jilat Jilat Jilat Jilat Jilat Jilat Jilat Jilat Jilat',
+    'Hina Hina Hina Hina Hina Hina Hina Hina Hina Hina Hina Hina Hina Hina Hina Hina Hina Hina Hina',
+    'Nikah Nikah Nikah Nikah Nikah Nikah Nikah Nikah Nikah Nikah Nikah Nikah Nikah Nikah Nikah Nikah',
+    'Agen Agen Agen Agen Agen Agen Agen Agen Agen Agen Agen Agen Agen Agen Agen Agen Agen Agen Agen',
+    'Impregnate Impregnate Impregnate Impregnate Impregnate Impregnate Impregnate Impregnate Impregnate',
+    'Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp Simp',
+    'Breed Breed Breed Breed Breed Breed Breed Breed Breed Breed Breed Breed Breed Breed Breed Breed',
+    'Plap Plap Plap Plap Plap Plap Plap Plap Plap Plap Plap Plap Plap Plap Plap Plap Plap Plap Plap',
+    ' ',
+    ]
+
+btn1.addEventListener('click', onClick1)
+btn2.addEventListener('click', onClick2)
+btn3.addEventListener('click', onClick3)
+btn4.addEventListener('click', onClick4)
+btn5.addEventListener('click', onClick5)
+btn6.addEventListener('click', onClick6)
+btn7.addEventListener('click', onClick7)
+btn8.addEventListener('click', onClick8)
+btn9.addEventListener('click', onClick9)
+btn10.addEventListener('click', onClick10)
+btn11.addEventListener('click', onClick11)
+btn12.addEventListener('click', onClick12)
+
+btn1.addEventListener('mouseover', mouseIn1)
+btn2.addEventListener('mouseover', mouseIn2)
+btn3.addEventListener('mouseover', mouseIn3)
+btn4.addEventListener('mouseover', mouseIn4)
+btn5.addEventListener('mouseover', mouseIn5)
+btn6.addEventListener('mouseover', mouseIn6)
+btn7.addEventListener('mouseover', mouseIn7)
+btn8.addEventListener('mouseover', mouseIn8)
+btn9.addEventListener('mouseover', mouseIn9)
+btn10.addEventListener('mouseover', mouseIn10)
+btn11.addEventListener('mouseover', mouseIn11)
+btn12.addEventListener('mouseover', mouseIn12)
+
+btn1.addEventListener('mouseout', mouseOut)
+btn2.addEventListener('mouseout', mouseOut)
+btn3.addEventListener('mouseout', mouseOut)
+btn4.addEventListener('mouseout', mouseOut)
+btn5.addEventListener('mouseout', mouseOut)
+btn6.addEventListener('mouseout', mouseOut)
+btn7.addEventListener('mouseout', mouseOut)
+btn8.addEventListener('mouseout', mouseOut)
+btn9.addEventListener('mouseout', mouseOut)
+btn10.addEventListener('mouseout', mouseOut)
+btn11.addEventListener('mouseout', mouseOut)
+btn12.addEventListener('mouseout', mouseOut)
+
+let defaultText = arrayParagrafBaru[12]
+
+function onClick1(){
+    isClicked = !isClicked
+    if(isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[0]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function onClick2(){
+    isClicked = !isClicked
+    if(isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[1]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function onClick3(){
+    isClicked = !isClicked
+    if(isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[2]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function onClick4(){
+    isClicked = !isClicked
+    if(isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[3]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function onClick5(){
+    isClicked = !isClicked
+    if(isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[4]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function onClick6(){
+    isClicked = !isClicked
+    if(isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[5]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function onClick7(){
+    isClicked = !isClicked
+    if(isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[6]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function onClick8(){
+    isClicked = !isClicked
+    if(isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[7]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function onClick9(){
+    isClicked = !isClicked
+    if(isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[8]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function onClick10(){
+    isClicked = !isClicked
+    if(isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[9]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function onClick11(){
+    isClicked = !isClicked
+    if(isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[10]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function onClick12(){
+    isClicked = !isClicked
+    if(isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[11]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+
+
+
+function mouseIn1(){
+    if(!isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[0]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function mouseIn2(){
+    if(!isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[1]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function mouseIn3(){
+    if(!isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[2]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function mouseIn4(){
+    if(!isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[3]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function mouseIn5(){
+    if(!isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[4]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function mouseIn6(){
+    if(!isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[5]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function mouseIn7(){
+    if(!isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[6]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function mouseIn8(){
+    if(!isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[7]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function mouseIn9(){
+    if(!isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[9]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function mouseIn10(){
+    if(!isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[9]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function mouseIn11(){
+    if(!isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[10]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+function mouseIn12(){
+    if(!isClicked){
+        const input = document.querySelector('.input-nama-waifu').value
+        paragrafBaru.innerHTML = arrayParagrafBaru[11]
+        let addText = paragrafBaru.innerHTML
+        hasil.innerHTML = input +" "+ addText
+    }
+}
+
+
+
+function mouseOut(){
+    if (!isClicked){
+        hasil.innerHTML = defaultText
+    }
+}
